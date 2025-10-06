fc3407a3535150800d2693151c813930146883f2
stephenp0320 <stephen power>
2025-10-06 10:31:09 +0100

    change profile app port

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ import defaultErrHandler from "./errHandler/index.js";
 import { connectDB } from "./db/index.js";   
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
